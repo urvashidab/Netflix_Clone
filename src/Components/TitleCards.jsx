fdb98a388948c93c656c09d91a9c22bca7cd6f72
urvashidab <urvashi.dabgotra11@gmail.com>
2025-11-15 00:13:27 +0530

    Home page added

---
+++ src/Components/TitleCards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TitleCards = () => {
+  return <div></div>;
+};
+
+export default TitleCards;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="text-shadow-blue-900"></div>;
+};
+
+export default Home;
