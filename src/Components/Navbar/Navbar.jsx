1949876790fe9a2c768e9986dd29e5db9214dce8
urvashidab <urvashi.dabgotra11@gmail.com>
2025-11-18 09:52:18 +0530

    font style change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,20 +7,20 @@ import caret from "../../assets/caret_icon.svg";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between  gap-8 fixed w-full z-1 py-6 px-8 md:px-12 lg:px-14 xl:px-32 ">
+    <nav className="flex justify-between  gap-8 fixed w-full z-10 py-6 px-8 md:px-12 lg:px-14 xl:px-32 backdrop-blur-md ">
       {/* left side part */}
       <div className="flex justify-center items-center gap-8">
-        <img alt="logo" src={logo} className="w-30"></img>
+        <img alt="logo" src={logo} className="w-32"></img>
 
         {/* list */}
 
         <ul className="flex justify-center items-center gap-4">
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
-          <li>Browse by Languages</li>
+          <li className="nav-link">Home</li>
+          <li className="nav-link">TV Shows</li>
+          <li className="nav-link">Movies</li>
+          <li className="nav-link">New & Popular</li>
+          <li className="nav-link">My List</li>
+          <li className="nav-link">Browse by Languages</li>
         </ul>
       </div>
 
